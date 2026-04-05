0ccd91043865b7a88f6b01a2d9954a622b337c0b
alaminShaheen <alaminshaheen23@gmail.com>
2026-04-05 12:11:28 -0400

    updated npm scripts for client side

--- apps/web/src/trpc/Provider.tsx
+++ apps/web/src/trpc/Provider.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import { useState } from 'react';
 import { trpc } from './client';
-import { env } from '../env';
+import { env } from '@/env';
 
 export function TRPCProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
